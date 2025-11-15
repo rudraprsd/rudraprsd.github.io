fde0d4c3b86c82173c234e676718bf962a52ecc4
rudraprsd <srudra3110@gmail.com>
2025-11-15 17:31:45 +0530

    Blog: why this website
    draft 01

---
+++ src/data/blogs/why-this-website.ts
@@ -0,0 +1,16 @@
+import { Blog } from './types';
+
+export const whyThisWebsite: Blog = {
+  id: "why-this-website",
+  title: "Why This Website?",
+  description: "Exploring the purpose and motivation behind creating this website",
+  date: "2025-11",
+  tags: ["Motivation"],
+  draft: true,
+  content: `
+I am into computer since I got a computer at home when I was around 5 years old. From that time I was always using computer for various purposes like playing games, MS Paint. My father had a C programming book (I don't remember the name) but that was very thick book. Then I got to know for this computer thing you need to study a lot. By that time, computer is rare in houses in India. Lots of young people use to come to our house to use computer and they taught me a lot of things. They used to save music videos, games from internet using dungle internet connection. I learnt typing using typing tutor software. Then slowly I increased my typing speed about 60 WPM. Impressive right. I know. That is practice. I did typed everyday for hours.
+
+But after that from 4th standard to 12th standard, I stayed outside where I had no computer access. But still my interest for computer did not fade away.
+After 12th, when I got into IISER, I started learning cpp, c, python, html, css, a little bit js etc. I also learnt how cpu works, dbms, machine learning, data structures and algorithms etc. I am not that guy who only into a particular field of coding that is web development or app development or game development. I like to explore all the fields of computer science. I also like to read research papers especially in machine learning, artificial intelligence etc. I love the machine learning field. 
+`
+};
