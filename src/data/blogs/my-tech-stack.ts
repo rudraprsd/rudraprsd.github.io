d46202521881dc43f231feb26f5ff1971b1b228f
rudraprsd <srudra3110@gmail.com>
2025-12-05 11:17:15 +0530

    Blog: My tech stack
    draft-01

---
+++ src/data/blogs/my-tech-stack.ts
@@ -0,0 +1,31 @@
+import { Blog } from './types';
+
+export const myTechStack: Blog = {
+  id: "my-tech-stack",
+  title: "My Tech Stack",
+  description: "The current best tech stack, I do use in my everyday life.",
+  date: "2025-12",
+  tags: ["Productivity"],
+  draft: true,
+  content: `
+  Let's start from the base.
+  Operating System: Linux (currently), I will buy mac soon. Well for personal use, linux is not that good. I have used linux for 5 years. See, linux is good for heavy work, enterprises, server etc. You can customize anything and all. But since there are no good laptop vendors in this world, so only software cannot do all these things. And only OS is not going to help us. We need a proper software suites for certain work (personal) which is a nightmare in linux.
+  For a simple thing, you need to be a software engineer that too mid-level code. You will realize that you will spend most time in customizing and solving the existing problem than doing any actual work. 
+
+  Don't talk about windows please. Now, come to MacOS. I know it is a bit pricey and all. But see the value it is providing with that amount of money. It is incredible. I know some services are subscription based. But nowadays all are shifted to macOS so most of the open-source softwares are available for macOS and some are only available to macOS.
+  Apple makes their hardware, software and the value and experience it provides. It's Iphone's are not that much great but the macbook's are awesome.
+
+  So, TL;DR, macOS and Linux.
+
+  Now terminal.
+  - shell (zsh)
+  - excalidraw
+  - obsidian
+  - vim
+  - vscode
+  - Telegram
+  - apple music
+  - gmail
+  - LLMs (Currently top ones only, But grok and gemini is better. Sometime claude code.)
+`
+};
