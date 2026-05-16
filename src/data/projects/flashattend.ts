556c9335c72ffc43b54d09460d87cdf5e8b1fdb9
rudraprsd <srudra3110@gmail.com>
2026-05-16 16:37:53 +0530

    Project: FlashAttend

---
+++ src/data/projects/flashattend.ts
@@ -0,0 +1,17 @@
+import AwesomeBZ from '@/components/projects/AwesomeBZ';
+import { Project } from './types';
+
+export const awesomeBZ: Project = {
+  id: "flashAttend",
+  title: "FlashAttend",
+  description: "AI attendance. Fast. Real-time",
+  date: "07-05-2026",
+  tags: ["Attendance", "AI", "Computer Vision", "Entreprenur", "Startup"],
+  draft: true,
+  content: `
+  AI attendance. Fast. Real-time.
+
+Instant attendance, straight from your live video stream.
+
+`
+};
